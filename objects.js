32b781247256d3c6b8e990e780a9e9c660b054ba
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-20 16:53:59 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,6 @@ var playlist = {Adele: "Rolling in the Deep"};
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName] : songTitle } );
 }
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.artistName;
+}
